b9f9ad200b93ac806e79ef5a9431bbf5ef3605a8
Ranjeet <ranjeetmdl@gmail.com>
2020-09-03 18:09:35 +0530

    section 31 : POST call/save to DB implemented

--- src/main/java/com/luv2code/springdemo/daos/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/daos/CustomerDAOImpl.java
@@ -7,8 +7,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.luv2code.springdemo.entities.Customer;
 
 @Repository
@@ -24,13 +22,22 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 		//create a query
 		Query<Customer> theQuery = 
-				session.createQuery("from Customer", Customer.class);
+				session.createQuery("from Customer order by lastName", Customer.class);
 		
 		//execute the query & get the list of customers
 		List<Customer> customers = theQuery.getResultList();
 		
 		//returns the list
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		// get the current session
+		Session session = sessionFactory.getCurrentSession();
+		
+		//save the customer....finally LOL
+		session.save(theCustomer);
 	}
 
 }
