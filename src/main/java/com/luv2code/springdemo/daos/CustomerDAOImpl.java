7ae3cd1376f59b2669cda857974723e6839efb00
Ranjeet <ranjeetmdl@gmail.com>
2020-09-06 15:49:13 +0530

    section 37 : code added

--- src/main/java/com/luv2code/springdemo/daos/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/daos/CustomerDAOImpl.java
@@ -51,4 +51,16 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return theCustomer;
 	}
 
+	@Override
+	public void deleteCustomer(int theId) {
+		//get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//delete the object/customer based on the primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+		
+	}
+
 }
